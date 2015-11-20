1e121e2c32293f48c9cf1795eb8b0455f88d8005
Walen <michal.walenia@gmail.com>
2015-11-20 21:44:40 +0100

    poprawki modelu, adres endpointa, konektor

--- app/src/main/java/pl/siiletscode/droppr/model/Event.java
+++ app/src/main/java/pl/siiletscode/droppr/model/Event.java
@@ -1,7 +1,88 @@
 package pl.siiletscode.droppr.model;
 
+import android.location.Location;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by Walen on 2015-11-20.
  */
 public class Event {
+    private String name;
+    private Location location;
+    private Date eventDate;
+    private String description;
+    private List<User> guests;
+    private User host;
+    private int minParticipants;
+    private int maxParticipants;
+
+    public double getDistance(Location loc){
+        return location.distanceTo(loc);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<User> getGuests() {
+        return guests;
+    }
+
+    public void setGuests(List<User> guests) {
+        this.guests = guests;
+    }
+
+    public User getHost() {
+        return host;
+    }
+
+    public void setHost(User host) {
+        this.host = host;
+    }
+
+    public int getMinParticipants() {
+        return minParticipants;
+    }
+
+    public void setMinParticipants(int minParticipants) {
+        this.minParticipants = minParticipants;
+    }
+
+    public int getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public void setMaxParticipants(int maxParticipants) {
+        this.maxParticipants = maxParticipants;
+    }
 }
--- app/src/main/java/pl/siiletscode/droppr/model/User.java
+++ app/src/main/java/pl/siiletscode/droppr/model/User.java
@@ -4,4 +4,50 @@ package pl.siiletscode.droppr.model;
  * Created by Walen on 2015-11-20.
  */
 public class User {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String passwordHash;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 }
