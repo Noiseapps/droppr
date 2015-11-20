e9d34a92492840798eff43257c8754181e44e7e9
Tomasz Ścibiorek <noiseapps@gmail.com>
2015-11-20 19:51:14 +0100

    maps

---
+++ app/src/main/java/pl/siiletscode/droppr/DropprApplication.java
@@ -0,0 +1,11 @@
+package pl.siiletscode.droppr;
+
+import android.support.multidex.MultiDexApplication;
+
+import org.androidannotations.annotations.EApplication;
+
+@EApplication
+public class DropprApplication extends MultiDexApplication {
+
+
+}
