0af3feddbf24638e491072908a973eb0f501e7e0
Walen <michal.walenia@gmail.com>
2015-11-20 21:42:34 +0100

    Dodano interfejs do resta, implementacja in progress

---
+++ app/src/main/java/pl/siiletscode/droppr/RESTConnection/DropprAPI.java
@@ -0,0 +1,52 @@
+package pl.siiletscode.droppr.RESTConnection;
+
+import java.util.List;
+
+import pl.siiletscode.droppr.model.Event;
+import pl.siiletscode.droppr.model.User;
+import retrofit.http.Body;
+import retrofit.http.DELETE;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.PUT;
+import retrofit.http.Path;
+import rx.Observable;
+
+/**
+ * Created by Walen on 2015-11-20.
+ */
+public interface DropprAPI {
+    @POST("/api/users")
+    void addUser(@Body String email, @Body String passwordHash);
+
+    @GET("/api/events")
+    Observable<List<Event>> getEventList();
+
+    @GET("api/events/types")
+    Observable<List<String>> getEventTypes();
+
+    @GET("/api/events/{id}/users")
+    Observable<List<User>> getEventGuests(@Path("id") int eventId);
+
+    @GET("/api/users/{id}")
+    Observable<User> getUserById(@Path("id") int userId);
+
+    @PUT("/api/users/{id}")
+    void editUserById(@Path("id") int userId, @Body User user);
+
+    @GET("/api/events/{id}")
+    Observable<Event> getEventById(@Path("id") int eventId);
+
+    @POST("/api/events")
+    void createEvent(@Body Event event);
+
+    @PUT("/api/events/{id}")
+    void editEventById(@Path("id") int eventId, @Body Event event);
+
+    @DELETE("/api/events/{id}")
+    void deleteEventById(@Path("id") int eventId);
+
+    @DELETE("/api/events/{evtId}/users/{usrId}")
+    void removeUserFromEvent(@Path("evtId") int eventId, @Path("usrId") int userId);
+
+}
---
+++ app/src/main/java/pl/siiletscode/droppr/RESTConnection/DropprConnector.java
@@ -0,0 +1,7 @@
+package pl.siiletscode.droppr.RESTConnection;
+
+/**
+ * Created by Walen on 2015-11-20.
+ */
+public class DropprConnector {
+}
